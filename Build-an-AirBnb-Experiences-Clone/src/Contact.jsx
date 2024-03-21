c3a2d7dee7d9fe946e317622d24e517ba6bde74f
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-03-20 19:36:40 -0700

    latest updates-part 2

--- Build-an-AirBnb-Experiences-Clone/src/Contact.jsx
+++ Build-an-AirBnb-Experiences-Clone/src/Contact.jsx
@@ -1,15 +1,15 @@
-export default function Contact(props) {
+export default function Contact({img, name, phone, email}) {
     return(
         <div className="contact-card">
-            <img src={props.img}/>
-            <h3>{props.name}</h3>
+            <img src={img}/>
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src="./phone-icon.png" />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img src="./mail-icon.png" />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </div>
     )
--- Build-an-AirBnb-Experiences-Clone/src/Hero.jsx
+++ Build-an-AirBnb-Experiences-Clone/src/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
         <section className="hero">
             <img src="./photo-grid.png" className="hero--photo"></img>
             <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">oin unique interactive activities led by 
+            <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
